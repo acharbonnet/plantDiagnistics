0eff3d87efba3f1cc79f06dbf86ec968468a7bc2
acharbonnet <adriana.charbonnet@gmail.com>
2022-07-16 13:49:57 -0600

    restructured for JS decision tree, example system.

--- script.js
+++ script.js
@@ -2,25 +2,38 @@ const option1 = document.getElementById('option1')
 const option2 = document.getElementById('option2')
 const result = document.getElementById('result');
 const questionContainer = document.getElementById('question-set');
-const stage = document.getElementById('stage-name');
+const question = document.getElementById('question');
 
-// function displayResult()
+// q: {text: '', answer1: {text: '', nextQuestion: ''}, answer2: {text: '', nextQuestion: ''}},
 
-// option1.addEventListener('click', result)
+var qObject =
+{
+    q1: {text: 'Is your fruit sweet or sour?', answer1: {text: 'Sweet', nextQuestion: 'q2'}, answer2: {text: 'Sour', nextQuestion: 'q3'}},
+    q2: {text: 'Is your fruit an apple', answer1: {text: 'Yes', nextQuestion: ''}, answer2: {text: 'No', nextQuestion: ''}},
+    q3: {text: 'Is your fruit a lemon', answer1: {text: 'Yes', nextQuestion: ''}, answer2: {text: 'No', nextQuestion: ''}},
+}
 
-// function nextStep(text) {
-//     const elem = document.getElementById('result');
-//     elem.textContent = text;
-// }
+function setQuestion(questionNumber) {
+    // Setting question 1 on page load
+    question.textContent = qObject[questionNumber].text;
+    option1.textContent  = qObject[questionNumber].answer1.text;
+    option2.textContent  = qObject[questionNumber].answer2.text;
+  }
+  
 
-// option1.addEventListener('click', nextStep(step2info));
+setQuestion('q1');
+let currentQuestion = 'q1';
+
 
 option1.onclick = function() {
-    stage.textContent = 'stage 2A';
-    option1.textContent = 'option 3';
-    // document.getElementById("stage2a").style.display="block";
+    setQuestion(qObject[currentQuestion].answer1.nextQuestion);
+    currentQuestion = qObject[currentQuestion].answer1.nextQuestion;
 }
 
 option2.onclick = function() {
-    result.textContent = "We have text ladies and gentlemen";
+    setQuestion(qObject[currentQuestion].answer2.nextQuestion);
+    currentQuestion = qObject[currentQuestion].answer2.nextQuestion;
 }
+
+/* option1.onclick = setQuestion(q2);
+option2.onclick = setQuestion(q3); */
